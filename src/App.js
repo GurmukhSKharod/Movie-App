8e5350dc2999b751ecc2098ea949840e4d2819f7
Gurmukh_S_Kharod <gurmukhkharod@hotmail.com>
2022-03-09 14:40:56 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MovieCard from './MovieCard';
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+//5a74a332
+const API_URL = 'http://www.omdbapi.com?apikey=5a74a332'
+
+const App = () => {
+
+    //user looks up different movies, so different state
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
+
+    //fetch movies by title
+    const searchMovies = async (title) => {
+        //call api
+        const response = await fetch(`${API_URL}&s=${title}`);
+        //get api response
+        const data = await response.json();
+
+        setMovies(data.Search); //array of movies
+    }
+
+    //called at start
+    useEffect(() => {
+        searchMovies('Batman');
+    }, []); 
+
+    // using '{ }' to make dynamic code on a movie state change. Dynamically loop over movies array, fetched from and API, then passing each movie as a prop to MovieCard. 
+    return (
+        <div className="app">
+            <h1>Movie App</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e)=> setSearchTerm(e.target.value)}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={()=> searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0 
+                ? (
+                    <div className="container">
+                        {movies.map((movie)=> (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                ) 
+            }
+
+            
+        </div>
+    );
 }
 
 export default App;
